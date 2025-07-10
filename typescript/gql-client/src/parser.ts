41e7a8e2cdd6e97b650ad4c19723c1848e27b165
Vladimir Voitenko <vladimirdev635@gmail.com>
2025-07-10 20:45:07 +0500

    ts:gql-client:fix-parser

--- typescript/gql-client/src/parser.ts
+++ typescript/gql-client/src/parser.ts
@@ -1,15 +1,39 @@
 import { z } from 'zod/v4';
 import { ClientParser, Operation } from './types.js';
 
-export function createParser<TContext>(): ClientParser<TContext> {
+interface CreateParserOptions<TContext> {
+    onErrors: <T extends Operation>(
+        context: TContext,
+        operation: T,
+        response: Response,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        errors: any[]
+    ) => Promise<void> | void
+}
+
+const defaultParserOptions: CreateParserOptions<unknown> = {
+    onErrors(_, __, ___, errors) {
+        throw new Error(JSON.stringify(errors))
+    },
+}
+
+export function createParser<TContext>(
+    options: CreateParserOptions<TContext> = defaultParserOptions
+): ClientParser<TContext> {
     return {
         parseBody: async <T extends Operation>(
-            _: TContext,
+            context: TContext,
             operation: T,
             response: Response
         ) => {
+            const json = await response.json()
+            if (json.errors) {
+                await options.onErrors(
+                    context, operation, response, json.errors
+                )
+            }
             return operation.resultSchema.parse(
-                await response.json()
+                json.data
             ) as z.infer<T['resultSchema']>
         },
     }
