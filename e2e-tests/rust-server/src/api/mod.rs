ba5423d88f5da72909c8ba9a2418b4bb93097c75
Vladimir Voitenko <vladimirdev635@gmail.com>
2026-03-24 07:31:34 +0500

    e2e-tests:rust-server:fix

--- e2e-tests/rust-server/src/api/mod.rs
+++ e2e-tests/rust-server/src/api/mod.rs
@@ -1,5 +1,3 @@
-use std::sync::Arc;
-
 pub mod context;
 pub mod generated;
 pub mod scalar;
@@ -26,18 +24,69 @@ struct GraphqlResponseBody {
     errors: Vec<GraphqlError>,
 }
 
-#[actix_web::post("/graphql")]
-pub async fn graphql(
-    state: actix_web::web::Data<Arc<state::APIState>>,
-    json: actix_web::web::Json<GraphqlRequestBody>,
-) -> impl actix_web::Responder {
-    let data = json.0;
+pub async fn root_handler(
+    state: &state::APIState,
+    request: hyper::Request<hyper::body::Incoming>,
+) -> Result<hyper::Response<String>, std::convert::Infallible> {
+    match (request.method(), request.uri().path()) {
+        (&hyper::Method::POST, "/graphql") => graphql_handler(state, request).await,
+        _ => {
+            let mut not_found = hyper::Response::new("".to_string());
+            *not_found.status_mut() = hyper::StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+
+async fn read_body(
+    request: hyper::Request<hyper::body::Incoming>,
+) -> Result<Vec<u8>, hyper::Response<String>> {
+    let upper = (request.body()
+        as &dyn hyper::body::Body<Data = hyper::body::Bytes, Error = hyper::Error>)
+        .size_hint()
+        .upper()
+        .unwrap_or(u64::MAX);
+    if upper > 1024 * 64 {
+        let mut resp = hyper::Response::new("Body too big".to_string());
+        *resp.status_mut() = hyper::StatusCode::PAYLOAD_TOO_LARGE;
+        return Err(resp);
+    }
+
+    let whole_body = http_body_util::BodyExt::collect(request)
+        .await
+        .map_err(|e| {
+            let mut response = hyper::Response::new(e.to_string());
+            *response.status_mut() = hyper::StatusCode::BAD_REQUEST;
+            response
+        })?
+        .to_bytes();
+    Ok(whole_body.to_vec())
+}
+
+async fn graphql_handler(
+    state: &state::APIState,
+    request: hyper::Request<hyper::body::Incoming>,
+) -> Result<hyper::Response<String>, std::convert::Infallible> {
+    let body = match read_body(request).await {
+        Ok(body) => body,
+        Err(response) => {
+            return Ok(response);
+        }
+    };
+    let data = match serde_json_path_to_error::from_slice::<GraphqlRequestBody>(&body) {
+        Ok(data) => data,
+        Err(error) => {
+            let mut response = hyper::Response::new(error.to_string());
+            *response.status_mut() = hyper::StatusCode::BAD_REQUEST;
+            return Ok(response);
+        }
+    };
     let query = data.query;
     let operation_result = match libgql::executor::execute(
         &(),
-        &state.get_ref().graphql_registry,
-        &state.get_ref().graphql_resolvers_map,
-        &state.get_ref().graphql_parse_registry,
+        &state.graphql_registry,
+        &state.graphql_resolvers_map,
+        &state.graphql_parse_registry,
         &query,
         data.variables.map_or(libgql::executor::Values::new(), |v| {
             libgql::json::executor::ast::parse_variables_from_json(&v).unwrap()
@@ -49,7 +98,7 @@ pub async fn graphql(
         Ok(result) => result,
         Err(error) => match error {
             libgql::executor::Error::ExecutionErrors(errors) => {
-                return actix_web::HttpResponse::Ok().json(GraphqlResponseBody {
+                let body = match serde_json_path_to_error::to_string(&GraphqlResponseBody {
                     data: serde_json_path_to_error::Value::Null,
                     errors: errors
                         .into_iter()
@@ -58,22 +107,52 @@ pub async fn graphql(
                             path: gql_error.path,
                         })
                         .collect(),
-                });
+                }) {
+                    Ok(body) => body,
+                    Err(error) => {
+                        return Ok(build_response(
+                            error.to_string(),
+                            hyper::StatusCode::BAD_REQUEST,
+                        ));
+                    }
+                };
+                return Ok(build_response(body, hyper::StatusCode::OK));
             }
-            _ => return actix_web::HttpResponse::BadRequest().body(format!("{:?}", error)),
+            _ => {
+                return Ok(build_response(
+                    format!("{:?}", error),
+                    hyper::StatusCode::BAD_REQUEST,
+                ));
+            }
         },
     };
     match operation_result {
         libgql::executor::OperationResult::Immediate(result) => {
             let json_result =
                 libgql::json::executor::ast::serialize_values_to_json(&result).unwrap();
-            actix_web::HttpResponse::Ok().json(GraphqlResponseBody {
+            let body = match serde_json_path_to_error::to_string(&GraphqlResponseBody {
                 data: json_result,
                 errors: Vec::new(),
-            })
+            }) {
+                Ok(body) => body,
+                Err(error) => {
+                    return Ok(build_response(
+                        error.to_string(),
+                        hyper::StatusCode::BAD_REQUEST,
+                    ));
+                }
+            };
+            Ok(build_response(body, hyper::StatusCode::OK))
         }
-        libgql::executor::OperationResult::Stream(_) => {
-            actix_web::HttpResponse::BadRequest().body("Unexpected stream response")
-        }
+        libgql::executor::OperationResult::Stream(_) => Ok(build_response(
+            "Unexpected stream response".to_string(),
+            hyper::StatusCode::BAD_REQUEST,
+        )),
     }
+}
+
+fn build_response(body: String, status_code: hyper::StatusCode) -> hyper::Response<String> {
+    let mut response = hyper::Response::new(body);
+    *response.status_mut() = status_code;
+    response
 }
--- e2e-tests/rust-server/src/main.rs
+++ e2e-tests/rust-server/src/main.rs
@@ -4,8 +4,11 @@ mod api;
 
 const GRAPHQL_SERVER_SCHEMA: &str = include_str!("../../graphql/server-schema.json");
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let port = std::env::var("PORT").unwrap().parse().unwrap();
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], port));
+    let listener = tokio::net::TcpListener::bind(addr).await?;
     let state = Arc::new(api::state::APIState {
         graphql_resolvers_map: api::generated::create_resolvers_map(),
         graphql_parse_registry: api::generated::create_parse_registry(),
@@ -22,12 +25,25 @@ async fn main() -> std::io::Result<()> {
             registry
         },
     });
-    actix_web::HttpServer::new(move || {
-        actix_web::App::new()
-            .app_data(actix_web::web::Data::new(state.clone()))
-            .service(api::graphql)
-    })
-    .bind(("0.0.0.0", std::env::var("PORT").unwrap().parse().unwrap()))?
-    .run()
-    .await
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        let io = hyper_util::rt::TokioIo::new(stream);
+        let local_state = state.clone();
+
+        tokio::task::spawn_local(async move {
+            let s = local_state;
+            if let Err(err) = hyper::server::conn::http1::Builder::new()
+                .serve_connection(
+                    io,
+                    hyper::service::service_fn(async |request| {
+                        api::root_handler(&s, request).await
+                    }),
+                )
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
+    }
 }
